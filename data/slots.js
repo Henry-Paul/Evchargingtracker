838e5190c19c7e54662be3f08dbd12e70e401a08
Henry-Paul <164559529+Henry-Paul@users.noreply.github.com>
2025-10-16 15:07:28 +0530

    Create slots.js

---
+++ data/slots.js
@@ -0,0 +1,12 @@
+[
+  {"id": "A1", "status": "available"},
+  {"id": "A2", "status": "available"},
+  {"id": "A3", "status": "available"},
+  {"id": "A4", "status": "available"},
+  {"id": "A5", "status": "available"},
+  {"id": "B1", "status": "available"},
+  {"id": "B2", "status": "available"},
+  {"id": "B3", "status": "available"},
+  {"id": "B4", "status": "available"},
+  {"id": "B5", "status": "available"}
+]
